66aeebebb42969698b3d69b8dc7af3ae5f5fd55d
Monel Lindu <monel.redlind@gmail.com>
2025-07-16 18:37:37 +0700

    render experience page at admin

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -89,6 +89,7 @@ export default function AdminView(){
                 formData = {experienceViewFormData}
                 setFormData = {setExperienceViewFormData}
                 handleSaveData = {handleSaveData}
+                data = {allData?.experience}
             />
         },
         {
--- src/components/admin-view/experience/index.jsx
+++ src/components/admin-view/experience/index.jsx
@@ -35,11 +35,27 @@ const controls = [
     }
 ]
 
-export default function AdminExperienceView({formData, setFormData, handleSaveData}){
+export default function AdminExperienceView({formData, setFormData, handleSaveData, data}){
     console.log(formData);
-        return (
-                <div className="w-full">
+        return <div className="w-full">
                         <div className="bg-[#d7d7d7] shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                        
+                        <div className="mb-10 space-y-6">
+                            {data && data.length ? (
+                                data.map((item,index) => (
+                                    <div key={index} className="bg-[#ffffff] flex flex-col gap-2 p-6 rounded-lg shadow-md border border-green-600 hover:border-green-800 transition duration-300">
+                                        <p className="text-lg font-semibold text-gray-700">Position: {item.position}</p>
+                                        <p className="text-lg text-gray-700">Company: {item.company}</p>
+                                        <p className="text-lg text-gray-700">Duration: {item.duration}</p>
+                                        <p className="text-lg text-gray-700">Location: {item.location}</p>
+                                        <p className="text-lg text-gray-700">{item.jobprofile}</p>
+                                    </div>
+                                ))
+                            ) :
+                                <p className="text-center text-gray-600">No Job Experience Data Available</p>
+                            }
+                        </div>
+
                         <FormControls 
                             controls={controls}
                             formData={formData}
@@ -50,5 +66,5 @@ export default function AdminExperienceView({formData, setFormData, handleSaveDa
                         </button>
                     </div>
                 </div>
-            )
+
 }

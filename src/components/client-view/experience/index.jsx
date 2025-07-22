8db3462c96f1301801138a28854e41b8b5befa41
Monel Lindu <monel.redlind@gmail.com>
2025-07-22 12:41:17 +0700

    working on education section

--- src/components/client-view/experience/index.jsx
+++ src/components/client-view/experience/index.jsx
@@ -49,6 +49,47 @@ export default function ClientExperienceAndEducationView({educationData, experie
                         </div>
                     </AnimationWrapper>
                 </div>
+
+                <div className="flex flex-col gap-5">
+                    <AnimationWrapper className={"py-6 sm:py-16"}>
+                        <div className="flex flex-col justify-center items-center row-start-2 sm:row-start-1">
+                            <h1 className="leading-[70px] mb-4 text-3xl lg:text-4xl xl:text-5xl font-medium">
+                                {"My Education".split(" ").map((item,index)=>(
+                                    <span className={`${index === 1 ? "text-green-main" : "text-black"}`}>
+                                        {item}{" "}
+                                    </span>
+                                ))}
+                            </h1>
+                        </div>
+                    </AnimationWrapper>
+                    
+
+                    {/* education */}
+                    <AnimationWrapper>
+                        <div className="flex w-full">
+                            <motion.div className="container">
+                                <Timeline position="right">
+                                    {educationData && educationData.length ? educationData.map((educationItem)=>(
+                                        <TimelineItem>
+                                            <TimelineSeparator>
+                                                <TimelineDot className="bg-green-main" />
+                                                <TimelineConnector className="bg-green-main" />
+                                            </TimelineSeparator>
+                                            <TimelineContent>
+                                                <div className="border-[2px] p-4 rounded-[8px] border-green-main mt-[14px] ml-[16px]">
+                                                    <p className="font-bold">Year: { educationItem.year }</p>
+                                                    <p className="font-extrabold mt-2">College: { educationItem.college } </p>
+                                                    <p className="font-bold mt-2">Degree: { educationItem.degree }</p>
+                                                </div>
+                                            </TimelineContent>
+                                        </TimelineItem>
+                                    )) : null }
+
+                                </Timeline>
+                            </motion.div>
+                        </div>
+                    </AnimationWrapper>
+                </div>
             </div>
 
         </div>
